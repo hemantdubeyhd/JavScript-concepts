004f370a2b4bbedf5816aef020448dd156790bf8
hemantdubeyhd <hemantdubeyhd@gmail.com>
2025-08-05 11:18:52 +0930

    Selecting Elements + Event Listener + Variable Usage

---
+++ Selecting Elements + Event Listener + Variable Usage/script.js
@@ -0,0 +1,6 @@
+const title = document.getElementById('mainTitle');
+const button = document.getElementById('changeBtn');
+
+button.addEventListener('click', () =>{
+title.innerText = 'Hello me!, I\'m tryign to teleport!!';
+});
